df88265ad3a2d8b020e91a180f4a61c36d706454
Sany Bakir <sany.bakir@epita.fr>
2026-09-05 19:17:17 +0200

    feat: ajout de la mesure de temps pour du uptime.

--- sbkrstatus/Program.cs
+++ sbkrstatus/Program.cs
@@ -8,20 +8,26 @@ class Program
     static readonly HttpClient client = new HttpClient();
     static async Task Main(string[] args)
     {
-        
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
         try
         {
-            var response = await client.GetAsync("http://www.google.com");
+            var response = await client.GetAsync("https://gitbbbbb.sbkr.fr/");
             response.EnsureSuccessStatusCode(); // interet du try catch si execption on le catch (status != 2xx)
             string responseBody = await response.Content.ReadAsStringAsync();
 
             Console.WriteLine(response.StatusCode);
+            Console.WriteLine("Temps de la requete : " + stopwatch.ElapsedMilliseconds + " ms");
         }
         catch (HttpRequestException e)
         {
             Console.WriteLine("\nException Caught !");
             Console.WriteLine(e.StatusCode);
             Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            stopwatch.Stop();
         }
     }
 }
