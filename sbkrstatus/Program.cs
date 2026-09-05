08c2a5e72b112ff055da33d023d10b2d443c99e3
Sany Bakir <sany.bakir@epita.fr>
2026-09-05 19:05:52 +0200

    fix: print le code la response et non le contenue de la response.

--- sbkrstatus/Program.cs
+++ sbkrstatus/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace sbkrstatus;
+﻿using System.Diagnostics;
+
+namespace sbkrstatus;
 
 // Le client http doit être instancier une fois par appli au lieu de chauqe usage d'ou le static
 class Program
@@ -6,14 +8,14 @@ class Program
     static readonly HttpClient client = new HttpClient();
     static async Task Main(string[] args)
     {
-
+        
         try
         {
             var response = await client.GetAsync("http://www.google.com");
             response.EnsureSuccessStatusCode(); // interet du try catch si execption on le catch (status != 2xx)
             string responseBody = await response.Content.ReadAsStringAsync();
 
-            Console.WriteLine(responseBody);
+            Console.WriteLine(response.StatusCode);
         }
         catch (HttpRequestException e)
         {
