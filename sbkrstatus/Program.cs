3ffe76e96d0d8f3bd550bc5df9813a2bc23a939e
Sany Bakir <sany.bakir@epita.fr>
2026-09-05 18:21:30 +0200

    e

---
+++ sbkrstatus/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace sbkrstatus;
+
+// Le client http doit être instancier une fois par appli au lieu de chauqe usage d'ou le static
+class Program
+{
+    static readonly HttpClient client = new HttpClient();
+    static async Task Main(string[] args)
+    {
+
+        try
+        {
+            var response = await client.GetAsync("http://www.google.com");
+            response.EnsureSuccessStatusCode(); // interet du try catch si execption on le catch (status != 2xx)
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            Console.WriteLine(responseBody);
+        }
+        catch (HttpRequestException e)
+        {
+            Console.WriteLine("\nException Caught !");
+            Console.WriteLine(e.StatusCode);
+            Console.WriteLine(e.Message);
+        }
+    }
+}
